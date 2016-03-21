26f7e9fb8cc4d29c6f8b4bf20737b36a59166452
Asaia Palacios <17@p2-4.hbtn.io>
2016-03-21 16:26:01 -0700

    Disables button while processing

--- js/load_more.js
+++ js/load_more.js
@@ -2,16 +2,22 @@ function addStatuses(response) {
     // Appends statuses to page
     document.getElementById("allStatuses").innerHTML += response;
     // Triggers DomContentLoaded event
-    var DOMContentLoaded_event = document.createEvent("Event")
-    DOMContentLoaded_event.initEvent("DOMContentLoaded", true, true)
-    window.document.dispatchEvent(DOMContentLoaded_event)   
+    var DOMContentLoaded_event = document.createEvent("Event");
+    DOMContentLoaded_event.initEvent("DOMContentLoaded", true, true);
+    window.document.dispatchEvent(DOMContentLoaded_event);   
+    // Enables moreStatuses button
+    document.getElementById("moreStatuses").querySelector("input").disabled = false;
 }
 
 document.addEventListener("DOMContentLoaded", function(){
     // Changes cursor to a pointer when hovering over the "See more statuses" button
-    document.getElementById("moreStatuses").style.cursor = "pointer";
+    document.getElementById("moreStatuses").querySelector("input").style.cursor = "pointer";
     // Load more pages to page when button clicks
     document.getElementById("moreStatuses").addEventListener("click", function(){ 
+        // Disables more statuses button
+        this.querySelector("input").disabled = true;
+        // Changes cursor to 'no' symbol
+        this.querySelector("input").style.cursor = "not-allowed";
         // Call ajax function
         ajaxGet("statuses-1.html", addStatuses);
    });
