845a24edecf5e04de60ddd2f461325da4d134486
Asaia Palacios <17@p2-4.hbtn.io>
2016-03-21 15:30:35 -0700

    Factorized the ajax function. Updated load_more.js file to use function

--- js/load_more.js
+++ js/load_more.js
@@ -1,25 +1,18 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("moreStatuses").style.cursor = "pointer";
-    document.getElementById("moreStatuses").addEventListener("click", function(){
-      
-var xmlhttp;
-  if (window.XMLHttpRequest) {
-    xmlhttp = new XMLHttpRequest();
-  } else {
-    // code for older browsers
-    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-  }
-  xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      document.getElementById("allStatuses").innerHTML += xmlhttp.responseText;
-    
+function addStatuses(response) {
+    // Appends statuses to page
+    document.getElementById("allStatuses").innerHTML += response;
+    // Triggers DomContentLoaded event
     var DOMContentLoaded_event = document.createEvent("Event")
     DOMContentLoaded_event.initEvent("DOMContentLoaded", true, true)
     window.document.dispatchEvent(DOMContentLoaded_event)   
-    }
-  };  
-       
-  xmlhttp.open("GET", "statuses-1.html", true);
-  xmlhttp.send();      
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    // Changes cursor to a pointer when hovering over the "See more statuses" button
+    document.getElementById("moreStatuses").style.cursor = "pointer";
+    // Load more pages to page when button clicks
+    document.getElementById("moreStatuses").addEventListener("click", function(){ 
+        // Call ajax function
+        ajaxGet("statuses-1.html", addStatuses);
    });
 });
