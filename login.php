3af921e099fa0fc425006c746d612def276284bd
Richard Harris <38@p4-2.hbtn.io>
2016-04-09 17:59:20 -0700

    Added login form and updated index filename

---
+++ login.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>SoLife | Your Passport to Life</title>
+        <link rel="stylesheet" type="text/css" href="css/reset.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/midsize.css">
+        <link rel="stylesheet" type="text/css" href="css/smartphone.css">
+        <script src="js/google.js" type="text/javascript"></script>
+        <script src="js/toggle.js" type="text/javascript"></script>
+        <script src="js/post_a_status.js" type="text/javascript"></script>
+        <script src="js/reply.js" type="text/javascript"></script>
+        <script src="js/load_more.js" type="text/javascript"></script>
+        <script src="js/ajax.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <div id="container">
+            <header>
+                <div id="hleft">
+                    <div id="logo">
+                        <img src="http://www3.nd.edu/~rharris8/newsletter/logo.png" alt="SoLife"/>
+                    </div>
+                    <div id="tagline">
+                        <h1>Your Passport to Life.</h1>
+                    </div>
+                </div>
+                <div id="hright">
+                    <div id="account">
+                        <ul>
+                            <li><a href="#">Edit my profile</a></li>
+                            <li><a href="#">Logout</a></li>
+                        </ul>
+                    </div>
+                    <div id="links">
+                        <ul>
+                            <li><a href="index.html">Home</a></li>
+                            <li><a href="profile.html">Statuses</a></li>
+                            <li><a href="users.html">Users</a></li>
+                            <li><a href="octopus-fitness.html">About</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </header>
+            <main>
+              <form id="loginForm" method="post" action="index.php">
+                <label for="login">Login: </label>
+                <input name="login" type="text">
+                <label for="password"> Password: </label>
+                <input name="password" type="password">
+                <input type="submit" value="Submit">
+              </form>
+              <div id="beta">
+                <h2>Welcome to SoLife!</h2>
+                <p>You have a passport to our <strong>beta</strong> version. Please report any issues that you   experience.</p>
+              </div>
+            </main>
+            <footer>
+                <div id="footerLinks">
+                  <ul>
+                      <li><a href="index.html">Home</a></li>
+                      <li><a href="profile.html">Statuses</a></li>
+                      <li><a href="users.html">Users</a></li>
+                      <li><a href="octopus-fitness.html">About</a></li>
+                  </ul>
+                </div>
+                <p>2016 &copy; SoLife</p>
+            </footer>
+        </div>
+    </body>
+</html>
