1c8720852268d20b31464eafc2616358d069ca11
rickharris-dev <rickharris724@gmail.com>
2016-07-01 21:15:47 -0700

    Testing host name

--- file.php
+++ file.php
@@ -1,5 +1,6 @@
 <?php
 header('Content-Type: text/plain');
+echo gethostname();
 echo "Server IP: ".$_SERVER['SERVER_NAME'];
 echo "\nClient IP: ".$_SERVER['REMOTE_NAME'];
 echo "\nX-Forwarded-for: ".$_SERVER['HTTP_X_FORWARDED_FOR'];
